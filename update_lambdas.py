4e19379fefe270d37ff44278da693a22f30ca118
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-08-28 15:59:53 +0200

    Fixed scripts zipping and s3 key name.

--- update_lambdas.py
+++ update_lambdas.py
@@ -16,9 +16,9 @@ def is_python_script(s3_file: str) -> bool:
     return s3_file.endswith('.py')
 
 
-def create_zip_file(zip_path: str, file_path: str, uploaded_file: str):
+def create_zip_file(zip_path: str, file_path: str):
     with zipfile.ZipFile(zip_path, 'w') as zipf:
-        zipf.write(file_path, uploaded_file)
+        zipf.write(file_path, zip_path)
 
 
 def upload_file(zip_path: str):
@@ -48,11 +48,11 @@ def upload_lambdas():
                 base_name = get_base_name(uploaded_file)
                 file_path = get_file_path(root, uploaded_file)
                 zip_path = get_zip_path(base_name, root)
-                create_zip_file(zip_path, file_path, uploaded_file)
+                create_zip_file(zip_path, file_path)
                 upload_file(zip_path)
                 remove_file(zip_path)
 
-                print(f'File: {uploaded_file} has been uploaded successfully')
+                print(f'File: {zip_path} has been uploaded successfully')
 
     print('Files uploading finished')
 
