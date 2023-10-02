7df8dfad1d3fa858d3067bfbdd152107e4fab16e
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-10-02 20:05:40 +0200

    Refactored pressure_simulator.py

--- lambda_functions/simulator/pressure_simulator.py
+++ lambda_functions/simulator/pressure_simulator.py
@@ -1,73 +1,46 @@
-import json
 import os
 import random
 import time
-import uuid
-from abc import abstractmethod
 
-import boto3
+from pydantic import BaseModel
 
-sns_client = boto3.client('sns')
+from commons import SensorData, SnsClient
+
+sns_client = SnsClient()
 TOPIC_ARN = os.getenv('TOPIC_ARN')
 
 
-class Jsonable:
-    @abstractmethod
-    def to_json(self): pass
+class PressureData(BaseModel):
+    pressure: float
 
 
-def publish_to_sns(message: Jsonable):
-    response = sns_client.publish(
-        TopicArn=TOPIC_ARN,
-        Message=json.dumps(message.to_json()),
-        MessageGroupId=str(uuid.uuid4())
-    )
-    return response
+class Pressure(SensorData):
+    pressure: PressureData
 
 
-class PressureData(Jsonable):
-    def to_json(self):
-        return {
-            "pressure": self.pressure
-        }
-
-    def __init__(self, pressure: float):
-        self.pressure = pressure
-
-
-class SensorData(Jsonable):
-    def to_json(self):
-        return {
-            "label": self.label,
-            "timestamp": self.timestamp,
-            "pressure": self.pressure.to_json(),
-            'event_key': self.event_key
-        }
-
-    def __init__(self, timestamp: int,
-                 pressure: PressureData,
-                 label: str,
-                 event_key: str):
-        self.timestamp = timestamp
-        self.pressure = pressure
-        self.label = label
-        self.event_key = event_key
+class PressureInput(BaseModel):
+    min_pressure_noise: float
+    max_pressure_noise: float
+    base_pressure: float
+    label: str
+    event_key: str
 
 
 def generate_data(event, context):
-    min_pressure_noise = float(event['min_pressure_noise'])
-    max_pressure_noise = float(event['max_pressure_noise'])
-    base_pressure = float(event['base_pressure'])
-    label = event['label']
-    event_key = event['event_key']
+    input_data = PressureInput(**event)
 
     timestamp = int(time.time())
-    pressure = base_pressure + random.uniform(min_pressure_noise, max_pressure_noise)
+    pressure = input_data.base_pressure + random.uniform(input_data.min_pressure_noise,
+                                                         input_data.max_pressure_noise)
 
-    pressure_data = PressureData(pressure)
-    sensor_data = SensorData(timestamp, pressure_data, label, event_key)
+    pressure_data = PressureData(pressure=pressure)
+    sensor_data = Pressure(pressure=pressure_data,
+                           label=input_data.label,
+                           timestamp=timestamp,
+                           event_key=input_data.event_key)
 
-    sns_response = publish_to_sns(sensor_data)
+    sns_response = sns_client.publish(message=sensor_data,
+                                      topic_arn=TOPIC_ARN)
 
     return {
         "statusCode": 200,
