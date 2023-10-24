7b7fa0a80c313c4248c9cfcd2e96d322fe2183c6
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-10-24 13:43:21 +0200

    Implemented wait_for_all_nodes_ready.py

--- structure/functions/wait_for_all_nodes_ready.py
+++ structure/functions/wait_for_all_nodes_ready.py
@@ -1,34 +1,37 @@
 import time
+from typing import List
+
 from kubernetes import client, config
 
 config.load_kube_config()
+k8s_client = client.CoreV1Api()
 
-kube_client = client.CoreV1Api()
 
-def wait_for_node_ready(node_name, timeout_seconds=300):
-    start_time = time.time()
+def get_ready_nodes_count() -> int:
+    ready_count = 0
+    nodes = k8s_client.list_node().items
+    for node in nodes:
+        conditions: List = node.status.conditions
+        for condition in conditions:
+            if condition.type == 'Ready' and condition.status == 'True':
+                    ready_count = ready_count + 1
+    return ready_count
 
-    while time.time() - start_time < timeout_seconds:
-        try:
-            node = kube_client.read_node(node_name)
-            conditions = node.status.conditions
-            for condition in conditions:
-                if condition.type == "Ready" and condition.status == "True":
-                    print(f"Node {node_name} is ready!")
-                    return True
-        except Exception as e:
-            print(f"Error checking node status: {str(e)}")
 
-        time.sleep(5)
+required_ready_nodes = 2
 
-    print(f"Timeout: Node {node_name} not ready after {timeout_seconds} seconds.")
-    return False
 
-node_list = kube_client.list_node()
+def wait_for_all_nodes_ready() -> None:
+    while True:
+        ready_nodes_count = get_ready_nodes_count()
+        if ready_nodes_count >= required_ready_nodes:
+            print("All nodes ready")
+            break
+        else:
+            print(
+                f"Awaiting for {required_ready_nodes} ready nodes. Current number of ready nodes: {ready_nodes_count}")
+            time.sleep(10)
 
-for node in node_list.items:
-    node_name = node.metadata.name
-    print(f"Checking node: {node_name}")
-    if wait_for_node_ready(node_name):
-        print(f"Do something with the ready node: {node_name}")
 
+if __name__ == '__main__':
+    wait_for_all_nodes_ready()
