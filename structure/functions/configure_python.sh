723270e70f46fcf53c0f4d0fd7192d5e08363d5d
damiano <macmacroll@gmail.com>
2024-02-12 23:42:30 +0100

    Fixed bash scripts

--- structure/functions/configure_python.sh
+++ structure/functions/configure_python.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain factory --domain-owner 781648067507 --region eu-central-1 --query authorizationToken --output text`
 echo $CODEARTIFACT_AUTH_TOKEN
-pip3 config set global.extra-index-url https://aws:${CODEARTIFACT_AUTH_TOKEN}@factory-781648067507.d.codeartifact.eu-central-1.amazonaws.com/pypi/FactoryRepository/simple/
+pip3 config set global.extra-index-url https://aws:$CODEARTIFACT_AUTH_TOKEN@factory-781648067507.d.codeartifact.eu-central-1.amazonaws.com/pypi/FactoryRepository/simple/
 pip3 install pythoncommons
